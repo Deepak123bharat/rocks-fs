166a89ddacbad05c9862e9b83e297ceda1de1282
deepak123bharat <dsrdeepak8@gmail.com>
2021-06-16 19:06:04 +0530

    Initial commit

---
+++ src/core/hardcoded.lua
@@ -0,0 +1,6 @@
+return {
+SYSCONFDIR = "/home/dsr/Development/luarocksDev/luarocks/testrun/testing_lrprefix-5.3/etc/luarocks",
+LUA_DIR = "/usr/local",
+LUA_BINDIR = "/usr/local/bin",
+LUA_INTERPRETER = "lua",
+}
